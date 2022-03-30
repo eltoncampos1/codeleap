43a188f3d8658e82b0563a8273866871f725d2c5
eltoncampos1 <eltoncampos36@gmail.com>
2022-03-30 15:13:15 -0300

    fix: input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -3,5 +3,5 @@ import { forwardRef } from "react";
 import * as S from './styles'
 
 export const Input = forwardRef<HTMLInputElement>((props, ref) => {
-  return <S.Container  {...props} ref={ref} type="text" />;
+  return <S.Container {...props} ref={ref} type="text" />;
 });
